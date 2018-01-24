ca796538cccfa4b3975c15ef1328a6baa65c3141
Winston Weinert <winston@ml1.net>
2018-01-24 10:31:14 -0600

    cut out some iterations

--- euler/p009/p009.rkt
+++ euler/p009/p009.rkt
@@ -1,8 +1,8 @@
 #lang racket
 
 (for*/first ([a (in-range 1 1000)]
-             [b (in-range (add1 a) 1000)]
-             [c (in-range (add1 b) 1000)]
+             [b (in-range (add1 a) (- 1001 a))]
+             [c (in-range (add1 b) (- 1001.0 a b))]
              #:when (and (= 1000 (+ a b c))
                          (= (sqr c) (+ (sqr a) (sqr b)))))
   (* a b c))
